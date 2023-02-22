50ee9edf6763ce73b7f12e906b812793448df10f
CharnyshSv <sv.charnysh@gmail.com>
2023-02-22 17:11:55 +0300

    lesson_1

--- lesson_.py
+++ lesson_.py
@@ -1,6 +1,33 @@
-s = input("Введите значение: ")
-while not s.isdigit():
-    s = input("Введите число: ")
-    print(s)
+# def findLen_m(message):
+#     counter = 0
+#     for i in message:
+#         counter += 1
+#     return counter
+#
+# def findLen_k(key):
+#     counter = 0
+#     for i in key:
+#         counter += 1
+#     return counter
+
+a = input("Message: ")
+b = input("Key: ")
+binary_a = []
+binary_b = []
+for char in a:
+    binary = bin(ord(char))[2:]
+    binary_a.append(binary)
+for char in b:
+    binary = bin(ord(char))[2:]
+    binary_b.append(binary)
+if len(binary_a) == len(binary_b) and binary_a == binary_b:
+    print("Good!")
+else:
+    print("Oops!")
+
+
+
+
+
 
 
