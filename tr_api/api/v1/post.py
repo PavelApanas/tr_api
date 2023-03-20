dbbd5b0124964abb3bf639bdaa310bff39ce0f58
CharnyshSv <sv.charnysh@gmail.com>
2023-03-20 14:29:07 +0300

    123

--- tr_api/api/v1/post.py
+++ tr_api/api/v1/post.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Header, status, Query
+from fastapi import APIRouter, HTTPException, Header, status, Query, Depends
 from jose import jwt, JWTError
 from datetime import datetime
 
@@ -12,8 +12,7 @@ from core.settings import EXPIRE_JWT_TOKEN, TOKEN_TYPE, ALGORITHM, SECRET_KEY
 post_router = APIRouter(prefix='/post')
 
 
-@post_router.post('/', response_model=PostDetail, status_code=status.HTTP_201_CREATED)
-async def create_post(post_form: PostCreateForm, authorization: str = Header()):
+async def validate_token(authorization: str = Header()) -> User:
     if authorization.startswith(TOKEN_TYPE):
         authorization = authorization.split()[1]
         try:
@@ -23,17 +22,20 @@ async def create_post(post_form: PostCreateForm, authorization: str = Header()):
         else:
             user = await User.select(User.username == payload.get('sub'))
             if user:
-                user = user[0]
-                post = Post(**post_form.dict() | {'author_id': user.pk})
-                try:
-                    await post.save()
-                except IntegrityError:
-                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='post exists')
-                else:
-                    return PostDetail.from_orm(post)
-            else:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='user has blocked')
+                return user[0]
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='user has blocked')
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='invalid token type')
+
+
+@post_router.post('/', response_model=PostDetail, status_code=status.HTTP_201_CREATED)
+async def create_post(post_form: PostCreateForm, PostCreateForm, user: User = Depends(validate_token)):
+   post = Post(**post_form.dict() | {'author_id': user.pk})
+    try:
+        await post.save()
+    except IntegrityError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='post exists')
+    else:
+        return PostDetail.from_orm(post)
 
 
 @post_router.get('/', response_model=list[PostDetail])
@@ -55,65 +57,41 @@ async def post_detail(
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Post not found')
 
 
-@post_router.delete('/{post_id}',status_code=status.HTTP_202_ACCEPTED)
+@post_router.delete('/{post_id}', status_code=status.HTTP_202_ACCEPTED)
 async def delete_post(
         post_id: int = Query(
             ge=1,
             title='Post Unique ID'
         ),
-        authorization: str = Header()
+        user: User = Depends(validate_token)
 ):
-    if authorization.startswith(TOKEN_TYPE):
-        authorization = authorization.split()[1]
-        try:
-            payload = jwt.decode(authorization, SECRET_KEY, ALGORITHM)
-        except JWTError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='token invalid')
-        else:
-            user = await User.select(User.username == payload.get('sub'))
-            if user:
-                user = user[0]
-                post = await Post.get(post_id)
-                if post:
-                    if post.author_id == user.pk:
-                        await post.delete()
-                        return {'detail': 'Post deleted successfully'}
-                    raise HTTPException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail='you are not the author of the post')
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='user was blocked')
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='invalid token type')
+    post = await Post.get(post_id)
+    if post:
+        if post.author_id == user.pk:
+            await post.delete()
+            return {'detail': 'Post deleted successfully'}
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='you are not the author of the post')
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
 
-@post_router.put('/',status_code=status.HTTP_202_ACCEPTED)
+@post_router.put('/', status_code=status.HTTP_202_ACCEPTED)
 async def update_post(
         post_detail: PostDetail,
-        authorization: str = Header()
+        user: User = Depends(validate_token)
 ):
-    if authorization.startswith(TOKEN_TYPE):
-        authorization = authorization.split()[1]
-        try:
-            payload = jwt.decode(authorization, SECRET_KEY, ALGORITHM)
-        except JWTError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='token invalid')
-        else:
-            user = await User.select(User.username == payload.get('sub'))
-            if user:
-                user = user[0]
-                post = await Post.get(post_detail.pk)
-                if post:
-                    if post.author_id == user.pk:
-                        post.title = post_detail.title
-                        post.body = post_detail.body
-                        post.date_created = datetime.now()
-                        try:
-                            await post.save
-                        except IntegrityError:
-                            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='post exists')
-                        return PostDetail.from_orm(post)
-                    raise HTTPException(
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                        detail='you are not the author of the post')
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='user was blocked')
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='invalid token type')
+    post = await Post.get(post_detail.pk)
+    if post:
+        if post.author_id == user.pk:
+            post.title = post_detail.title
+            post.body = post_detail.body
+            post.date_created = datetime.now()
+            try:
+                await post.save
+            except IntegrityError:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='post exists')
+            return PostDetail.from_orm(post)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='you are not the author of the post')
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
---
+++ tr_api/core/utils/mail.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def send_mail(to: str, subject:str, message: str) -> None:
+    msg = MIMEMultipart()
+    msg['From'] = 'pavelfapanas@yandex.ru'
+    msg['To'] = to
+    msg['Subject'] = subject
+    msg.attach(MIMEText(message))
+    server = smtplib.SMTP('smtp.yandex.ru', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login('pavelfapanas@yandex.ru', 'fgtfgfedivnbuuaz')
+    server.sendmail('pavelfapanas@yandex.ru', to, msg.as_string())
+
+
